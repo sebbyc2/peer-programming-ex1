ed6f3997f73b32268fd5cfe99f665b390e429b13
sebby <sebbyc2@gmail.com>
2022-01-19 11:37:25 +0000

    Added Factorial class with method reverse

--- src/main/java/com/qa/main/Factorial.java
+++ src/main/java/com/qa/main/Factorial.java
@@ -2,4 +2,16 @@ package com.qa.main;
 
 public class Factorial {
 
+	public static String reverse(double target) {
+
+		for (int i = 1; target >= 1; i++) {
+			target /= i;
+			if (target == 1) {
+				return i + "!";
+			}
+		}
+
+		return "NONE";
+	}
+
 }
